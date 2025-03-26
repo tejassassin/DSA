500c80ea9c2841ba14766d58e05e61fd2dd33abb
tejassassin <tejamallela2000@gmail.com>
2025-03-26 11:27:38 +0530

    find peaks using binary search

---
+++ BinarySearch/3.Find_Peak_Ele/1.BruteForce.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(N), N = size of the given array.
+// Reason: We are traversing the entire array.
+
+// Space Complexity: O(1) as we are not using any extra space.
+
+int findPeakElement(vector<int> &arr) {
+    int n = arr.size(); //Size of array.
+
+    for (int i = 0; i < n; i++) {
+        //Checking for the peak:
+        if ((i == 0 || arr[i - 1] < arr[i])
+                && (i == n - 1 || arr[i] > arr[i + 1])) {
+            return i;
+        }
+    }
+    // Dummy return statement
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 5, 1};
+    int ans = findPeakElement(arr);
+    cout << "The peak is at index: " << ans << "\n";
+    return 0;
+}
+
